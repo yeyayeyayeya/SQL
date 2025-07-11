964785489774a9774558eb0fcbd579d0733048f4
yeyayeyayeya <165641921+yeyayeyayeya@users.noreply.github.com>
2025-07-11 15:20:13 +0700

    Update config.py

--- Cassandra_logging/ETL_Casandra/config.py
+++ Cassandra_logging/ETL_Casandra/config.py
@@ -1,12 +1,13 @@
+# The names (of driver, server, host,...) may vary/differ depending on each device or set by the user.
 SQL_SERVER_CONFIG = {
-    "driver": "ODBC Driver 17 for SQL Server",
-    "server": "DESKTOP-7COGVJA\\XUANYEN",
+    "driver": "ODBC Driver 17 for SQL Server",  
+    "server": "DESKTOP-7COGVJA\\XUANYEN", 
     "database": "SV",
-    "trusted_connection": "yes",
+    "trusted_connection": "yes", # in case of using Windows Authentication
 }
 
 CASSANDRA_CONFIG = {
-    "hosts": ["192.168.174.128"],
+    "hosts": ["192.168.174.128"], 
     "port": 9042,
     "keyspace": "sv_keyspace",
 }
