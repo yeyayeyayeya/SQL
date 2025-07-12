6475bf4ce3a3d9bbedc4ac63faf594213e77b94e
yeyayeyayeya <165641921+yeyayeyayeya@users.noreply.github.com>
2025-07-12 22:10:58 +0700

    Update config.py

--- StudentManagement/config.py
+++ StudentManagement/config.py
@@ -1,10 +1,10 @@
 # config.py
-
+# The names (of driver, server, host,...) may vary/differ depending on each device or set by the user.
 SQL_SERVER_CONN_STR = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=DESKTOP-7COGVJA\\XUANYEN;"
     "DATABASE=SV;"
-    "Trusted_Connection=yes;"
+    "Trusted_Connection=yes;" #in case using Windows Authentication
 )
 
 POSTGRES_CONN_STR = "postgresql+psycopg2://postgres:yen@localhost:5432/ql_diem"
